0d1258d57d1234c24f1c65f892a4837e17d6f67f
王扬赞 <wang.yangzan@lmf.com>
2020-05-22 18:05:15 +0800

    新增netty相关

---
+++ src/main/java/com/rabbitmq/rabbitmqdemo/netty/NettyServerHandler.java
@@ -0,0 +1,44 @@
+package com.rabbitmq.rabbitmqdemo.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author wyz
+ */
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 读取数据事件
+     *
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("Server : " + ctx);
+        ByteBuf byteBuf = (ByteBuf) msg;
+        System.out.println("客户端发来的消息 ：" + byteBuf.toString(CharsetUtil.UTF_8));
+    }
+
+    /**
+     * 数据读取完毕事件
+     *
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("就是没钱", CharsetUtil.UTF_8));
+    }
+}
